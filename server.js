104c09b2a9589d066c7be439e84bf85a7e7fe604
kk77777 <kaushikgattani@gmail.com>
2020-03-09 23:54:19 +0530

    changes for heroku deployment

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Connection to MongoDb database established successfully');
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on port : ${port}`);
+});
